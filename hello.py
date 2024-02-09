027032ea4a7741debe18ccd17398667ce2e70ac2
ganman2205 <140781880+ganman2205@users.noreply.github.com>
2024-02-09 10:59:03 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+
+    return app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    create_app().run(debug=True)
